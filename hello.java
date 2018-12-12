e46d0fb33c9fe9e3c13a883fb961700b584f40ff
nooris azima <noorisazima@gmail.com>
2018-12-12 10:41:35 +0530

    intial commit

---
+++ hello.java
@@ -0,0 +1,11 @@
+//for testing 
+
+
+class Hello-world
+{  
+    public static void main(String args[])
+    {  
+    System.out.println("Hello world");  
+    System.out.println("i am in local");
+    }  
+}  
